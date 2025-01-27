2c6dea06f5e2bbf88084fcdcae12d4f46cdc2e35
jatinag17 <jatinagrawal128@gmail.com>
2025-01-27 11:20:03 +0530

    update operation

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,5 +1,10 @@
 import prisma from "../DB/db.config.js";
 
+export const fetchUser=async(req,res)=>{
+    const users=await prisma.user.findMany({})
+
+    return res.json({ status: 200, data: users, msg: "Users fetched." });
+}
 export const createUser=async(req,res)=>{
     const{name,email,password}=req.body
     const findUser=await prisma.user.findUnique({
@@ -13,6 +18,7 @@ export const createUser=async(req,res)=>{
           message: "Email Already Taken . please another email.",
         });
       }
+      //create opertaion
        const newUser = await prisma.user.create({
          data: {
            name: name,
@@ -22,3 +28,32 @@ export const createUser=async(req,res)=>{
        });
  return res.json({ status: 200, data: newUser, msg: "User created." });
 };
+
+// Show user
+
+export const showUser=async(req,res)=>{
+  const userid=req.params.id
+  const user=await prisma.user.findFirst({
+    where: {
+        id:Number(userid)
+    },
+  })
+  return res.json({ status: 200, data:user})
+};
+
+// update the user
+export const updateUser = async(req,res)=>{
+    const userId=req.params.id 
+    const { name, email, password } = req.body;
+await prisma.user.update({
+    where: {
+        id:Number(userId)
+    },
+    data: {
+        name,
+        email,
+        password
+    }
+})
+    return res.json({ status: 200, msg: "User updated successfully." });
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,13 @@
 import {Router} from 'express'
-import { createUser } from '../controller/UserController.js'
+import { createUser,updateUser,fetchUser,showUser } from '../controller/UserController.js'
 
 const router = Router()
 
 router.post("/", createUser)
+router.put("/:id", updateUser)
+router.get("/:id", fetchUser)
+router.get("/:id", showUser)
+
+
+
 export default router
